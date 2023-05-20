e9344a7460a32687daaf744da6ccae3bd2116c6e
Gordon Lee <guodun.li@gmail.com>
2023-05-20 12:13:16 +0000

    update arxiv

--- arxiv.py
+++ arxiv.py
@@ -1,6 +1,7 @@
 
 '''
 credit to original author: Glenn (chenluda01@outlook.com)
+Author: Doragd
 '''
 
 import os
@@ -10,6 +11,30 @@ import json
 import datetime
 from tqdm import tqdm
 
+SERVERCHAN_API_KEY = os.environ.get("SERVERCHAN_API_KEY", None)
+QUERY = os.environ.get('QUERY', 'cs.IR')
+LIMITS = os.environ.get('LIMITS', 3)
+CAIYUN_TOKEN = os.environ.get("CAIYUN_TOKEN", None)
+
+def translate(source, direction='en2zh'):
+    url = "http://api.interpreter.caiyunai.com/v1/translator"
+
+    payload = {
+        "source": source,
+        "trans_type": direction,
+        "request_id": "demo",
+        "detect": True,
+    }
+
+    headers = {
+        "content-type": "application/json",
+        "x-authorization": "token " + CAIYUN_TOKEN,
+    }
+    try:
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+        return json.loads(response.text)["target"]
+    except:
+        return []
 
 def get_yesterday():
     today = datetime.datetime.now()
@@ -51,8 +76,13 @@ def search_arxiv_papers(search_term, max_results=10):
             'url': url,
             'pub_date': pub_date,
             'summary': summary,
+            'translated': '',
         })
+    
+    print('[+] 开始翻译每日最新论文并缓存....')
 
+    papers = save_and_translate(papers)
+    
     return papers
 
 
@@ -65,46 +95,73 @@ def send_wechat_message(title, content, SERVERCHAN_API_KEY):
     requests.post(url, params=params)
 
 
-def save_to_local_file(papers, filename='arxiv.json'):
+def save_and_translate(papers, filename='arxiv.json'):
     with open(filename, 'r', encoding='utf-8') as f:
         results = json.load(f)
 
-    titles = {paper['title'].lower() for paper in results}
-    add_papers = [paper for paper in papers if paper['title'].lower() not in titles]
-
-    results.extend(add_papers)
+    cached_title2idx = {result['title'].lower():i for i, result in enumerate(results)}
+    
+    untranslated_papers = []
+    translated_papers = []
+    for paper in papers:
+        title = paper['title'].lower()
+        if title in cached_title2idx.keys():
+            translated_papers.append(
+                results[cached_title2idx[title]]
+            )
+        else:
+            untranslated_papers.append(paper)
+    
+    source = []
+    for paper in untranslated_papers:
+        source.append(paper['summary'])
+    target = translate(source)
+    if len(target) == len(untranslated_papers):
+        for i in range(len(untranslated_papers)):
+            untranslated_papers[i]['translated'] = target[i]
+    
+    results.extend(untranslated_papers)
 
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(results, f, indent=4, ensure_ascii=False)
 
+    print(f'[+] 总检索条数: {len(papers)} | 命中缓存: {len(translated_papers)} | 实际返回: {len(untranslated_papers)}....')
+
+    return untranslated_papers # 只需要发送缓存中没有的
+
         
-
-
 def cronjob():
-
-    SERVERCHAN_API_KEY = os.environ.get("SERVERCHAN_API_KEY", None)
 
     if SERVERCHAN_API_KEY is None:
         raise Exception("未设置SERVERCHAN_API_KEY环境变量")
 
-    search_term = os.environ.get('QUERY', 'cs.IR')
-    max_results = os.environ.get('THREADS', 1)
-
     print('[+] 开始执行每日推送任务....')
-    yesterday = get_yesterday()
-    print('[+] 开始检索每日最新论文....')
-    papers = search_arxiv_papers(search_term, max_results)
 
-    save_to_local_file(papers)
+    yesterday = get_yesterday()
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    print('[+] 开始检索每日最新论文....')
+    papers = search_arxiv_papers(QUERY, LIMITS)
+
+    if papers == []:
+        
+        push_title = f'Arxiv:{QUERY}[X]@{today}'
+        send_wechat_message('', '[WARN] NO UPDATE TODAY!', SERVERCHAN_API_KEY)
+
+        print('[+] 每日推送任务执行结束')
+
+        return True
+        
 
     print('[+] 开始推送每日最新论文....')
 
-    for paper in tqdm(papers, total=len(papers), desc=f"论文推送进度"):
+    for ii, paper in enumerate(tqdm(papers, total=len(papers), desc=f"论文推送进度")):
 
         title = paper['title']
         url = paper['url']
         pub_date = paper['pub_date']
         summary = paper['summary']
+        translated = paper['translated']
 
         yesterday = get_yesterday()
 
@@ -116,16 +173,18 @@ def cronjob():
         msg_url = f'URL: {url}'
         msg_pub_date = f'Pub Date：{pub_date}'
         msg_summary = f'Summary：\n\n{summary}'
+        msg_translated = f'Translated:\n\n{translated}'
 
-        today = datetime.datetime.now().strftime('%Y-%m-%d')
-        push_title = f'Arxiv:{search_term}@{today}'
-        msg_content = f"[{msg_title}]({url})\n\n{msg_pub_date}\n\n{msg_url}\n\n{msg_summary}\n\n"
+        push_title = f'Arxiv:{QUERY}[{ii}]@{today}'
+        msg_content = f"[{msg_title}]({url})\n\n{msg_pub_date}\n\n{msg_url}\n\n{msg_translated}\n\n{msg_summary}\n\n"
 
         send_wechat_message(push_title, msg_content, SERVERCHAN_API_KEY)
 
         time.sleep(12)
 
     print('[+] 每日推送任务执行结束')
+
+    return True
 
 
 if __name__ == '__main__':
