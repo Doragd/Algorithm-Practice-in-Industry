6e8befc96e7022d038e031e42e3cf3566f60b5ea
Gordon Lee <guodun.li@gmail.com>
2024-04-21 14:11:11 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -95,7 +95,7 @@ def parse_item(item):
 def update_results(results):
     # confs = ['kdd', 'cikm', 'sigir', 'www', 'wsdm', 'ecir', 'recsys']
     confs = ['www', 'sigir', 'cikm', 'kdd',  'wsdm', 'recsys', 'ecir']
-    years = list(range(2023, 2010, -1))
+    years = list(range(2024, 2010, -1))
     count = 0
     daily_limits = False
     ret_items = []
